d98ff12553306872c82d9f2c5349a583edc5cabc
Aryan <aryan0y7@gmail.com>
2026-08-12 05:51:22 +0530

    third project

--- JsAdvanced/3-DigitalClock/script.js
+++ JsAdvanced/3-DigitalClock/script.js
@@ -1,87 +1,12 @@
-// const body = document.querySelector('body');
-// const buttons = document.querySelectorAll('.color-button');
-
-// buttons.forEach(btn =>{
-//     btn.addEventListener('click',event=>{
-
-//         // why didn't we use event.target.style.color
-//         // because this only works for inline html 
-//         // for this we need getComputedStyle from css sheet cssom
-//         const styles = getComputedStyle(event.target);
-//         document.body.style.backgroundColor=
-//         styles.backgroundColor;
-//     })
-// })
+const clock = document.getElementById("clock");
 
 
-/* 
-so getComputedStyle is not from cssom but from 
-computed styles layer after the render tree
-                HTML
-                 |
-                 ↓
-                DOM Tree
-                 |
-                 |
-                CSS
-                 |
-                 ↓
-                CSSOM Tree
-                 |
-                 ↓
-                Browser combines DOM + CSSOM
-                 |
-                 ↓
-                Render Tree
-                 |
-                 ↓
-                Computed Styles
-                 |
-                 ↓
-                Layout + Paint
-*/
+// now how do i run this continously ?
+// we have a js method called setInterval()
+// give me method and interval after how much i should run it continously
 
 
-// another method
-
-/* 
-
-
-body {
-  --bg-color: white;
-  background-color: var(--bg-color);
-}suppose this is the css
-
-
-
-btn.addEventListener('click', event => {
-    const styles = getComputedStyle(event.target);
-
-    document.body.style.setProperty(
-        "--bg-color",
-        styles.backgroundColor
-    );
-});
-*/
-
-
-
-
-// the best method - event delegation and event bubbling 
-
-const parent = document.querySelector('.color-changer');
-// get the parent attach event listner here
-// imagine 100 children running foreach through node list reduces performance
-// more memory so instead event delegation
-// but group-accordingly as it recives clicks from everything inside parent even heading or paragraph
-// this is sites put transparent pages above with settimeout and evenlistners 
-// this is how scammers use eventlistners - they add a transparent overlay 
-// add settimeout repeat and event.target
-// window.location.href="some-site.com";
-
-
-parent.addEventListener('click',event=>{
-    const style = getComputedStyle(event.target);
-    document.body.style.backgroundColor=
-    style.backgroundColor;
-})
+setInterval(() => {
+    let date = new Date();
+    clock.innerHTML=`${date.toLocaleTimeString()}`;
+}, 1000);
