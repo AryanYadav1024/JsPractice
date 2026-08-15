8d7af6f07b3c7424927c9b15a7849d8cb6774399
Aryan yadav <143953035+AryanYadav1024@users.noreply.github.com>
2026-08-15 22:41:18 +0530

    add structure and response (#6)
    * add structure and layout
    
    * update style
    
    * add update
    
    * add responsive design
    
    * add responsive design

---
+++ JsAdvanced/07_script.js
@@ -0,0 +1,5 @@
+const heading = document.querySelector("h1")
+
+heading.addEventListener("click",event=>{
+    window.location.href="https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Events";
+})
