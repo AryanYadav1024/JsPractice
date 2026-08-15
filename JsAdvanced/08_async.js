83ddd39a74a14d7310a82e2638a45c1bccea85b7
Aryan <aryan0y7@gmail.com>
2026-08-16 02:44:50 +0530

    add async js file

---
+++ JsAdvanced/08_async.js
@@ -0,0 +1,13 @@
+/*
+    So javascript is single threaded and synchronous, meaning that it can only do one thing at a time. 
+    However, it can handle asynchronous operations using callbacks, promises, and async/await. 
+    This allows JavaScript to perform tasks like fetching data from an API or reading files without blocking the main thread.
+
+
+    An execution context is an abstract environment created by the JavaScript engine to evaluate and execute code.
+    Think of it as a container or a workspace that holds all the variables, functions, and information needed for the
+    current code to run. Everything in JavaScript executes inside an execution context.
+
+    line executes one by one 
+    
+*/
