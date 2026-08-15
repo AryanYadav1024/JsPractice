a233a0c006f1a39e14412ef381f692e5456169b6
Aryan <aryan0y7@gmail.com>
2026-08-15 22:39:21 +0530

    add responsive design

--- JsAdvanced/07_script.js
+++ JsAdvanced/07_script.js
@@ -3,4 +3,3 @@ const heading = document.querySelector("h1")
 heading.addEventListener("click",event=>{
     window.location.href="https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Events";
 })
-
