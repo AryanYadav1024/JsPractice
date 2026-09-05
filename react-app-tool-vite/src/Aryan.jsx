ef4ded2867c4a086a92dd93d1f0bcb38e9c54bd8
Aryan <aryan0y7@gmail.com>
2026-09-05 11:48:45 +0530

    how render works fragment return import export

---
+++ react-app-tool-vite/src/Aryan.jsx
@@ -0,0 +1,12 @@
+function Aryan(){
+    return(
+        // now you can't do multiple elements you can only return one element inside it can be multiple
+        // before people used to return divs but now you can return fragment
+        // <> </>
+        <>
+        <h1>Rendering our own file</h1>
+        <p>Returning multiple elements using fragment</p>
+        </>
+    )
+}
+export default Aryan
