fa10940d56932ab427e0cf3e2b71baa61f261f40
Aryan <aryan0y7@gmail.com>
2026-09-06 23:39:57 +0530

    batch updates error what is fiber how react hook queue works

--- tailwindProps/src/App.jsx
+++ tailwindProps/src/App.jsx
@@ -10,13 +10,23 @@ function App() {
   const [counter2, setCount2] = useState(0)
   function addValue(setter,value){
     setter(value + 1)
+    setter(value + 1)
+    setter(value + 1)
+    setter(value + 1)
+    // now this will produce a batch but and scheduled in a hook queue with each one having same state to effectively they do not work as imagined
+    // there we have function value pass 
+    // setter(c => c + 1) 1
+    // setter(c => c + 1) 2
+    // this is a method react provides -> abstraction internally it has code to implement this
+    // let newState = function(state){return state + 1} then passes on to another and so on
   }
   return (
     <>
     <h1 className="text-3xl font-bold italic">
       Hello Tailwind!
     </h1>
-    {/* passing prop to child component button from parent component */}
+    {/* passing prop to child component button from parent component
+        ()=> addValue() -> this internally evaluates to a function */}
     <Button addValue={()=> addValue(setCount,counter)} counter={counter}/>
     <Button addValue={()=> addValue(setCount2,counter2)} counter={counter2}/>
     </> 
--- tailwindProps/src/components/button.jsx
+++ tailwindProps/src/components/button.jsx
@@ -1,6 +1,6 @@
 // button function component accepts props from App componenet (parent)
 
-function Button({addValue,counter}){ 
+function Button({addValue,counter=5}){ 
     return(
     <button onClick={addValue} className="w-32 mb-3 mx-auto bg-blue-500 hover:bg-red-500 text-white px-5 py-3 rounded-lg transition-colors duration-300">
        Click me
