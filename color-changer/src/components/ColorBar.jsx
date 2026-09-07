4ab77c73804b746791fb7ac0add364cd0be4346c
Aryan <aryan0y7@gmail.com>
2026-09-07 15:39:16 +0530

    color changer using react very Hard

--- color-changer/src/components/ColorBar.jsx
+++ color-changer/src/components/ColorBar.jsx
@@ -11,6 +11,10 @@ function ColorBar({changeColor}){
     ];
     return(
         <div className="color-bar">
+            {/* jsx syntax we have to return the mapped elements of list
+                also while passing list item as a prop we need to pass key for 
+                react to associate the element and do orderwise work This was also described
+                in fiber document */}
             {cards.map((item) => {
                 return (
                     <ColorCard 
